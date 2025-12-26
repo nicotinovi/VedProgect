01ba9f33956f69fc65340a5adbde85b473b66ddb
nicotinovi <basmanovsemens@gmail.com>
2025-12-26 23:32:58 +0800

    plus3

--- backend/app/route/teacher.js
+++ backend/app/route/teacher.js
@@ -1,29 +1,34 @@
 module.exports = (app) => {
-
     const teacher = require('../controller/teacher');
 
     // получение списка преподавателей
-    app.get('/api/listTeachers', teacher.findAll);
+
+    app.get('/api/listTeachers', teacher.listTeachers);
 
     // добавление данных преподавателя
-    app.post('/api/addTeacher', teacher.create);
+
+    app.post('/api/addTeacher', teacher.addTeacher);
 
     // обновление данных преподавателя
-    app.post('/api/updateTeacher/:id', teacher.update);
+
+    app.post('/api/updateTeacher/:id', teacher.updateTeacher);
 
     // удаление данных преподавателя
-    app.post('/api/deleteTeacher/:id', teacher.delete);
+
+    app.post('/api/deleteTeacher/:id', teacher.deleteTeacher);
 
     // получение данных преподавателя по id
     app.get('/api/teacher/:id', teacher.findById);
 
     // получения информации о том, какие учебные дисциплины ведут преподаватели
-    app.get('/api/listTeacherDiscipline', teacher.findDisciplines);
+
+    app.get('/api/listTeacherDiscipline', teacher.listTeacherDiscipline);
 
     // назначение преподавателю учебной дисциплины
-    app.post('/api/addTeacherDiscipline', teacher.createDisciplines);
+
+    app.post('/api/addTeacherDiscipline', teacher.addTeacherDiscipline);
 
     // удаление информации о назначении преподавателю учебной дисциплины
-    app.post('/api/deleteTeacherDiscipline/teacherId=:teacher_id/disciplineId=:discipline_id', teacher.deleteDisciplines);
 
+    app.post('/api/deleteTeacherDiscipline/teacherId=:teacher_id/disciplineId=:discipline_id', teacher.deleteTeacherDiscipline);
 };
